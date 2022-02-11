400190361e846fd1fb1ebf0830b9dd641d16fd16
Cristian Manzo <ca.manzo973@uniandes.edu.co>
2022-02-11 14:46:23 -0500

    feat:  add create_dict function

--- kata.js
+++ kata.js
@@ -15,3 +15,18 @@ const arr3 = {'A': null,
             const arr4 = {'t': {'tr': {'tru': {'true': null, 'trus': {'trust': null}}}}}
             
 const split = (str,num_split=0) => str.slice(0,num_split) ;
+
+
+const create_dict =(raw_str='',level_tree=1)=>{
+    const str=raw_str.trim();
+    const len_str = str.length;
+    let dict={};
+    if (len_str ==0){ return dict; };
+    if (len_str==level_tree){
+        dict[split(str,len_str)]=null;
+        return dict;
+    };
+        dict[split(str,len_str-1)]=split(str,len_str);
+        console.log(split(str,len_str-1));
+        return dict;
+}
