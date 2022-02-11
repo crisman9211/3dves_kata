5bb91f2f0a88341261f7af1b1f58cd23bd98feb9
Cristian Manzo <ca.manzo973@uniandes.edu.co>
2022-02-11 15:27:45 -0500

    feat: add str_to_arr and tree function transform string to array path transform string to mapNode obj

--- kata.js
+++ kata.js
@@ -53,3 +53,43 @@ const reduce_str =(raw_str,level_tree)=>{
     resp = split(str,level_tree+1);
     return resp;
 }
+
+const str_to_arr=(myStr,level_tree=1)=>{
+    let arr =[];
+    let iter=myStr.length-1;
+    let key = myStr;
+    while(iter>=0){
+        let value = reduce_str(myStr,iter);
+        // console.log(value)
+        arr.push(value);
+        key= value;
+        iter--;
+    }
+    if (level_tree>1){     
+        let max_remove=1;
+        while (max_remove<=level_tree-1){
+           arr.pop();
+           max_remove++;
+        }
+    }
+    return arr;
+}
+
+
+const tree = (str, level=1)=>{
+    let arr= str_to_arr(str,level);
+    let i=0;
+    let myNode = new mapNode();
+    while(i<str.length+1-level){
+        let myTree_prev = new mapNode();
+        if(i==0){
+            myTree_prev.set(str,arr[0]);
+        }
+        else{
+            myTree_prev.set(arr[i],myNode.collection);
+        }
+        myNode=myTree_prev;
+        i++;
+    }
+    return myNode;
+}
