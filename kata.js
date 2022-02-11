e53ca21692fa366841a0dcfbcfa06f88c0fa79cc
Cristian Manzo <ca.manzo973@uniandes.edu.co>
2022-02-11 14:42:58 -0500

    feat:  add split string function

---
+++ kata.js
@@ -0,0 +1,17 @@
+const arr0 ={'t': {'tr': {'tri': {'trie': null}}}};
+
+const arr2 = {'t': {'tr': {'tre': {'tree': null}}}};
+
+const arr3 = {'A': null,
+'t': {'to': null,
+                    'te': { 'tea': null,
+                            'ted': null,
+                            'ten': null
+                        }
+                    },
+              'i': {'in': {'inn': null}}
+            };
+            
+            const arr4 = {'t': {'tr': {'tru': {'true': null, 'trus': {'trust': null}}}}}
+            
+const split = (str,num_split=0) => str.slice(0,num_split) ;
